b4f6b59cddb9d02da147f0d3b94ecdee454314c0
Sanchez <107179906+AliaksandrZurausky@users.noreply.github.com>
2026-01-09 21:38:42 +0300

    Языковые файлы - правильная структура

---
+++ install/lang/ru/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$MESS['BITRIX_MIGRATOR_NAME'] = 'Мигратор Bitrix';
+$MESS['BITRIX_MIGRATOR_DESCRIPTION'] = 'Миграция данных из облака в коробку';
+$MESS['BITRIX_MIGRATOR_INSTALL_TITLE'] = 'Установка модуля';
+$MESS['BITRIX_MIGRATOR_UNINSTALL_TITLE'] = 'Удаление модуля';
+$MESS['BITRIX_MIGRATOR_MIN_VERSION_ERROR'] = 'Требуется Bitrix версия 19.0.0 или выше';
+$MESS['BITRIX_MIGRATOR_HL_REQUIRED'] = 'Требуется модуль High Load Block';
+$MESS['BITRIX_MIGRATOR_INSTALL_SUCCESS'] = 'Модуль успешно установлен';
+$MESS['BITRIX_MIGRATOR_INSTALL_FAILED'] = 'Ошибка при установке';
+$MESS['BITRIX_MIGRATOR_UNINSTALL_SUCCESS'] = 'Модуль успешно удален';
+$MESS['BITRIX_MIGRATOR_UNINSTALL_FAILED'] = 'Ошибка при удалении';
+$MESS['BITRIX_MIGRATOR_RETURN_MODULES'] = '← Вернуться к модулям';
+$MESS['BITRIX_MIGRATOR_DELETE_DATA'] = 'Удалить все данные миграции (очереди и логи)';
+$MESS['BITRIX_MIGRATOR_DELETE_DATA_HINT'] = 'Если не отмечено, таблицы будут сохранены';
+$MESS['BITRIX_MIGRATOR_CONFIRM_DELETE'] = 'Удалить модуль';
+$MESS['BITRIX_MIGRATOR_CANCEL'] = 'Отмена';
