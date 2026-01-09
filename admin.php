be4af040472653e37cc6b0af626c525f9beb6312
Sanchez <107179906+AliaksandrZurausky@users.noreply.github.com>
2026-01-09 20:50:13 +0300

    Add admin.php for proper access routing

---
+++ admin.php
@@ -0,0 +1,11 @@
+<?php
+
+// Этот файл должен быть в корне модуля
+// и определят админ-страницы
+
+$ADMIN_FILE = [
+    'PAGE_TITLE' => 'Bitrix Migrator',
+    'FILE' => 'admin/bitrix_migrator.php',
+    'SORT' => 150,
+    'ICON' => 'icon',
+];
