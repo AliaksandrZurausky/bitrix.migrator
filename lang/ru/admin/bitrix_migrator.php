b8ffd5448bd0401af894900832e7d5f9eaf8d3aa
Sanchez <107179906+AliaksandrZurausky@users.noreply.github.com>
2026-01-10 14:31:47 +0300

    fix: Move admin language files to correct path
    Move language files from lang/*/install/admin/ to lang/*/admin/
    so Bitrix can automatically load them for admin pages

---
+++ lang/ru/admin/bitrix_migrator.php
@@ -0,0 +1,24 @@
+<?php
+
+$MESS['BITRIX_MIGRATOR_MODULE_NOT_INSTALLED'] = 'Модуль Bitrix Migrator не установлен';
+$MESS['BITRIX_MIGRATOR_PAGE_TITLE'] = 'Bitrix Migrator - Миграция данных';
+
+$MESS['BITRIX_MIGRATOR_TAB_SETTINGS'] = 'Настройки';
+$MESS['BITRIX_MIGRATOR_TAB_SETTINGS_TITLE'] = 'Настройки подключения к облаку';
+$MESS['BITRIX_MIGRATOR_TAB_STATE'] = 'Состояние';
+$MESS['BITRIX_MIGRATOR_TAB_STATE_TITLE'] = 'Текущее состояние миграции';
+$MESS['BITRIX_MIGRATOR_TAB_LOGS'] = 'Логи';
+$MESS['BITRIX_MIGRATOR_TAB_LOGS_TITLE'] = 'Журнал миграции';
+
+$MESS['BITRIX_MIGRATOR_CLOUD_URL'] = 'URL облачного портала';
+$MESS['BITRIX_MIGRATOR_CLOUD_WEBHOOK'] = 'Входящий вебхук';
+$MESS['BITRIX_MIGRATOR_CLOUD_WEBHOOK_HINT'] = 'Формат: 1/abc123def456 (без https://your-portal.bitrix24.ru/rest/)';
+
+$MESS['BITRIX_MIGRATOR_STATE_INFO'] = 'Здесь отображается текущее состояние миграции';
+$MESS['BITRIX_MIGRATOR_STATE_STATUS'] = 'Статус';
+$MESS['BITRIX_MIGRATOR_STATE_NOT_STARTED'] = 'Не запущена';
+
+$MESS['BITRIX_MIGRATOR_LOGS_INFO'] = 'Здесь будут отображаться логи миграции';
+
+$MESS['BITRIX_MIGRATOR_BTN_SAVE'] = 'Сохранить';
+$MESS['BITRIX_MIGRATOR_SETTINGS_SAVED'] = 'Настройки успешно сохранены';
