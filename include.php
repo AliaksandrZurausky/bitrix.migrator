08b1f2eee4dd2b8650acad10345559a3f702167d
Sanchez <107179906+AliaksandrZurausky@users.noreply.github.com>
2026-01-10 14:28:38 +0300

    fix: Add EventHandlers to autoload for menu registration

--- include.php
+++ include.php
@@ -5,11 +5,12 @@ use Bitrix\Main\Loader;
 Loader::registerAutoloadClasses(
     'bitrix_migrator',
     array(
-        'BitrixMigrator\Agent\MigratorAgent' => 'lib/Agent/MigratorAgent.php',
-        'BitrixMigrator\Service\StateService' => 'lib/Service/StateService.php',
-        'BitrixMigrator\Service\QueueService' => 'lib/Service/QueueService.php',
-        'BitrixMigrator\Service\MapService' => 'lib/Service/MapService.php',
-        'BitrixMigrator\Service\LogService' => 'lib/Service/LogService.php',
-        'BitrixMigrator\Integration\CloudAPI' => 'lib/Integration/CloudAPI.php',
+        'BitrixMigrator\\Agent\\MigratorAgent' => 'lib/Agent/MigratorAgent.php',
+        'BitrixMigrator\\Service\\StateService' => 'lib/Service/StateService.php',
+        'BitrixMigrator\\Service\\QueueService' => 'lib/Service/QueueService.php',
+        'BitrixMigrator\\Service\\MapService' => 'lib/Service/MapService.php',
+        'BitrixMigrator\\Service\\LogService' => 'lib/Service/LogService.php',
+        'BitrixMigrator\\Integration\\CloudAPI' => 'lib/Integration/CloudAPI.php',
+        'BitrixMigrator\\EventHandlers' => 'lib/EventHandlers.php',
     )
 );
