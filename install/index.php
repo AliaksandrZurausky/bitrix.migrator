023e24e1685e38a82a16fb2243c56f42bc8e1c3c
Sanchez <107179906+AliaksandrZurausky@users.noreply.github.com>
2026-01-10 13:17:58 +0300

    feat(block-1): Add MigratorState, MigratorMap, and extended MigratorLogs HL-blocks creation

--- install/index.php
+++ install/index.php
@@ -100,8 +100,107 @@ class bitrix_migrator extends CModule
             throw new Exception(Loc::getMessage('BITRIX_MIGRATOR_HL_REQUIRED'));
         }
 
+        $this->createStateTable();
         $this->createQueueTable();
+        $this->createMapTable();
         $this->createLogsTable();
+    }
+
+    public function createStateTable()
+    {
+        $hlblock = \Bitrix\Highloadblock\HighloadBlockTable::getList([
+            'filter' => ['=NAME' => 'MigratorState']
+        ])->fetch();
+
+        if (!$hlblock) {
+            $result = \Bitrix\Highloadblock\HighloadBlockTable::add([
+                'NAME' => 'MigratorState',
+                'TABLE_NAME' => 'b_migrator_state'
+            ]);
+
+            if (!$result->isSuccess()) {
+                throw new Exception('Error creating MigratorState table: ' . implode(', ', $result->getErrorMessages()));
+            }
+
+            $blockId = $result->getId();
+            $userTypeEntity = new \CUserTypeEntity();
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_STATUS',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_STATUS',
+                'MANDATORY' => 'Y',
+                'EDIT_FORM_LABEL' => ['ru' => 'Статус миграции'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Статус'],
+                'SETTINGS' => ['SIZE' => 50]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_CURRENT_PHASE',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_CURRENT_PHASE',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Текущая фаза'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Фаза'],
+                'SETTINGS' => ['SIZE' => 50]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_PAUSE_FLAG',
+                'USER_TYPE_ID' => 'boolean',
+                'XML_ID' => 'UF_PAUSE_FLAG',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Флаг паузы'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'На паузе']
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_SETTINGS_JSON',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_SETTINGS_JSON',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Настройки (JSON)'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Настройки'],
+                'SETTINGS' => ['SIZE' => 0, 'ROWS' => 5]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_LAST_ERROR',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_LAST_ERROR',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Последняя ошибка'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Ошибка'],
+                'SETTINGS' => ['SIZE' => 0, 'ROWS' => 3]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_STARTED_AT',
+                'USER_TYPE_ID' => 'datetime',
+                'XML_ID' => 'UF_STARTED_AT',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Начало миграции'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Начало']
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_UPDATED_AT',
+                'USER_TYPE_ID' => 'datetime',
+                'XML_ID' => 'UF_UPDATED_AT',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Обновлено'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Обновлено']
+            ]);
+
+            Option::set($this->MODULE_ID, 'state_hlblock_id', $blockId);
+        }
     }
 
     public function createQueueTable()
@@ -136,12 +235,23 @@ class bitrix_migrator extends CModule
 
             $userTypeEntity->Add([
                 'ENTITY_ID' => 'HLBLOCK_' . $blockId,
-                'FIELD_NAME' => 'UF_ENTITY_ID',
-                'USER_TYPE_ID' => 'integer',
-                'XML_ID' => 'UF_ENTITY_ID',
+                'FIELD_NAME' => 'UF_CLOUD_ID',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_CLOUD_ID',
                 'MANDATORY' => 'Y',
-                'EDIT_FORM_LABEL' => ['ru' => 'ID сущности'],
-                'LIST_COLUMN_LABEL' => ['ru' => 'ID']
+                'EDIT_FORM_LABEL' => ['ru' => 'Cloud ID'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Cloud ID'],
+                'SETTINGS' => ['SIZE' => 100]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_LOCAL_ID',
+                'USER_TYPE_ID' => 'integer',
+                'XML_ID' => 'UF_LOCAL_ID',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Local ID'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Local ID']
             ]);
 
             $userTypeEntity->Add([
@@ -149,7 +259,7 @@ class bitrix_migrator extends CModule
                 'FIELD_NAME' => 'UF_STATUS',
                 'USER_TYPE_ID' => 'string',
                 'XML_ID' => 'UF_STATUS',
-                'MANDATORY' => 'N',
+                'MANDATORY' => 'Y',
                 'EDIT_FORM_LABEL' => ['ru' => 'Статус'],
                 'LIST_COLUMN_LABEL' => ['ru' => 'Статус'],
                 'SETTINGS' => ['SIZE' => 20]
@@ -157,26 +267,135 @@ class bitrix_migrator extends CModule
 
             $userTypeEntity->Add([
                 'ENTITY_ID' => 'HLBLOCK_' . $blockId,
-                'FIELD_NAME' => 'UF_CLOUD_ID',
+                'FIELD_NAME' => 'UF_RETRIES',
                 'USER_TYPE_ID' => 'integer',
-                'XML_ID' => 'UF_CLOUD_ID',
+                'XML_ID' => 'UF_RETRIES',
                 'MANDATORY' => 'N',
-                'EDIT_FORM_LABEL' => ['ru' => 'Cloud ID'],
-                'LIST_COLUMN_LABEL' => ['ru' => 'Cloud ID']
+                'EDIT_FORM_LABEL' => ['ru' => 'Попытки'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Попытки']
             ]);
 
             $userTypeEntity->Add([
                 'ENTITY_ID' => 'HLBLOCK_' . $blockId,
-                'FIELD_NAME' => 'UF_ERROR_MSG',
+                'FIELD_NAME' => 'UF_LAST_ERROR',
                 'USER_TYPE_ID' => 'string',
-                'XML_ID' => 'UF_ERROR_MSG',
+                'XML_ID' => 'UF_LAST_ERROR',
                 'MANDATORY' => 'N',
-                'EDIT_FORM_LABEL' => ['ru' => 'Сообщение об ошибке'],
+                'EDIT_FORM_LABEL' => ['ru' => 'Последняя ошибка'],
                 'LIST_COLUMN_LABEL' => ['ru' => 'Ошибка'],
                 'SETTINGS' => ['SIZE' => 0, 'ROWS' => 3]
             ]);
 
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_PRIORITY',
+                'USER_TYPE_ID' => 'integer',
+                'XML_ID' => 'UF_PRIORITY',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Приоритет'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Приоритет']
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_DEPENDS_ON',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_DEPENDS_ON',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Зависит от'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Зависит от'],
+                'SETTINGS' => ['SIZE' => 100]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_UPDATED_AT',
+                'USER_TYPE_ID' => 'datetime',
+                'XML_ID' => 'UF_UPDATED_AT',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Обновлено'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Обновлено']
+            ]);
+
             Option::set($this->MODULE_ID, 'queue_hlblock_id', $blockId);
+        }
+    }
+
+    public function createMapTable()
+    {
+        $hlblock = \Bitrix\Highloadblock\HighloadBlockTable::getList([
+            'filter' => ['=NAME' => 'MigratorMap']
+        ])->fetch();
+
+        if (!$hlblock) {
+            $result = \Bitrix\Highloadblock\HighloadBlockTable::add([
+                'NAME' => 'MigratorMap',
+                'TABLE_NAME' => 'b_migrator_map'
+            ]);
+
+            if (!$result->isSuccess()) {
+                throw new Exception('Error creating MigratorMap table: ' . implode(', ', $result->getErrorMessages()));
+            }
+
+            $blockId = $result->getId();
+            $userTypeEntity = new \CUserTypeEntity();
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_ENTITY_TYPE',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_ENTITY_TYPE',
+                'MANDATORY' => 'Y',
+                'EDIT_FORM_LABEL' => ['ru' => 'Тип сущности'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Тип'],
+                'SETTINGS' => ['SIZE' => 50]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_CLOUD_ID',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_CLOUD_ID',
+                'MANDATORY' => 'Y',
+                'EDIT_FORM_LABEL' => ['ru' => 'Cloud ID'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Cloud ID'],
+                'SETTINGS' => ['SIZE' => 100]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_LOCAL_ID',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_LOCAL_ID',
+                'MANDATORY' => 'Y',
+                'EDIT_FORM_LABEL' => ['ru' => 'Local ID'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Local ID'],
+                'SETTINGS' => ['SIZE' => 100]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_CLOUD_URL',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_CLOUD_URL',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Cloud URL'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Cloud URL'],
+                'SETTINGS' => ['SIZE' => 255]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_META_JSON',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_META_JSON',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Метаданные (JSON)'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Метаданные'],
+                'SETTINGS' => ['SIZE' => 0, 'ROWS' => 3]
+            ]);
+
+            Option::set($this->MODULE_ID, 'map_hlblock_id', $blockId);
         }
     }
 
@@ -232,6 +451,27 @@ class bitrix_migrator extends CModule
                 'SETTINGS' => ['SIZE' => 500]
             ]);
 
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_PAYLOAD',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_PAYLOAD',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Данные (JSON)'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Данные'],
+                'SETTINGS' => ['SIZE' => 0, 'ROWS' => 3]
+            ]);
+
+            $userTypeEntity->Add([
+                'ENTITY_ID' => 'HLBLOCK_' . $blockId,
+                'FIELD_NAME' => 'UF_CREATED_AT',
+                'USER_TYPE_ID' => 'datetime',
+                'XML_ID' => 'UF_CREATED_AT',
+                'MANDATORY' => 'N',
+                'EDIT_FORM_LABEL' => ['ru' => 'Создано'],
+                'LIST_COLUMN_LABEL' => ['ru' => 'Создано']
+            ]);
+
             Option::set($this->MODULE_ID, 'logs_hlblock_id', $blockId);
         }
     }
@@ -242,7 +482,7 @@ class bitrix_migrator extends CModule
             return;
         }
 
-        $tables = ['MigratorQueue', 'MigratorLogs'];
+        $tables = ['MigratorState', 'MigratorQueue', 'MigratorMap', 'MigratorLogs'];
 
         foreach ($tables as $tableName) {
             $hlblock = \Bitrix\Highloadblock\HighloadBlockTable::getList([
